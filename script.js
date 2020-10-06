79dac7eebc61a1b013c26f19365394c717a5d16e
Jimbiscus <Hasborae@gmail.com>
2020-10-06 14:24:40 +0200

    learning random and practicing git

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 console.log("JIMBERT");
 alert("ERROR");
 
+
+function getRandomInt() {
+    return Math.floor(Math.random() * 10);
+}
+
+
 var x = "Jo"
-var y = Math.floor(Math.random() * 11);
+var y = Math.floor(Math.random() * 110459);
 
 console.log("Salut " + x + ". Je t'envoie " + y + " bisous via internet")
+console.log("Salut " + x + ". Je t'envoie " + getRandomInt() + " bisous via internet")
