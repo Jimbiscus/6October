55f967bd8674bf3a2f78653e51123b5fad823917
Jimbiscus <Hasborae@gmail.com>
2020-10-06 15:54:25 +0200

    refactored

--- exo2.js
+++ exo2.js
@@ -8,31 +8,26 @@ function discount(x) {
 }
 
 function discountMachine() {
-    
+    console.log(x + "% de réduction.");
+    console.log("Le prix de votre article : " + depense + "€");
+    console.log("Votre réduction : " + discount(x) + "€");
+    console.log("Votre prix final :" + discountedPrice(x) + "€");
 }
 
 if (depense < 20) {
     console.log("Pas de réduction.");
-   
+
 }
 else if (depense >= 20 && depense <= 30) {
     x = 10;
-    console.log("10% de réduction.");
-    console.log("Le prix de votre article : " + depense + "€");
-    console.log("Votre réduction : " + discount(x) + "€");
-    console.log("Votre prix final :" + discountedPrice(x) + "€");
+    discountMachine();
+
 }
 else if (depense > 30 && depense <= 50) {
     x = 20;
-    console.log("20% de réduction.");
-    console.log("Le prix de votre article : " + depense + "€");
-    console.log("Votre réduction : " + discount(x) + "€");
-    console.log("Votre prix final :" + discountedPrice(x) + "€");
+    discountMachine();
 }
 else {
     x = 30;
-    console.log("30% de réduction.");
-    console.log("Le prix de votre article : " + depense + "€");
-    console.log("Votre réduction : " + discount(x) + "€");
-    console.log("Votre prix final :" + discountedPrice(x) + "€");
+    discountMachine();
 }
