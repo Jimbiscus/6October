ff447bfd922d5f790e027b37695380095eb50b06
Jimbiscus <Hasborae@gmail.com>
2020-10-08 10:57:36 +0200

    Login system

---
+++ exo4.js
@@ -0,0 +1,11 @@
+console.log("Jim");
+console.error("ERROR : YOU NEED MORE PIZZAS");
+alert("Ceci est une alerte !");
+window.confirm("Vous allez pouvoir confirmer ou annuler !");
+var test = window.prompt("Entrez votre mot de passe : ");
+if (test === "t0t0t1t1") {
+    document.write("Bienvenue Toto !");
+}
+else {
+    document.write("MDP Incorrect !")
+}
